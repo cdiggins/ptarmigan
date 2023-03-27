da9eec0fc62f04600f212960ff44d4d41c01e24d
Christopher Diggins <cdiggins@gmail.com>
2023-03-27 02:13:25 -0400

    Adding more functions:

--- src/Ptarmigan.Utils.Roslyn/RoslynAnalysisUtils.cs
+++ src/Ptarmigan.Utils.Roslyn/RoslynAnalysisUtils.cs
@@ -5,6 +5,8 @@ using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Ptarmigan.Utils.Roslyn
 {
@@ -59,7 +61,19 @@ namespace Ptarmigan.Utils.Roslyn
             }
         }
 
-        public static IEnumerable<ISymbol> GetAllSymbols(this Compilation compilation)
+        public static IEnumerable<ISymbol> GetDeclaredAndBaseMembers(this ITypeSymbol sym)
+        {
+            foreach (var m in sym.GetMembers())
+                yield return m;
+            if (sym.BaseType != null)
+                foreach (var m in sym.BaseType.GetDeclaredAndBaseMembers())
+                    yield return m;
+        }
+
+        public static IEnumerable<ITypeSymbol> GetFieldTypes(this ITypeSymbol sym)
+            => sym.GetDeclaredAndBaseMembers().OfType<IFieldSymbol>().Select(f => f.Type);
+
+        public static IEnumerable<ISymbol> GetExpressionAndTypeSymbols(this Compilation compilation)
         {
             foreach (var st in compilation.SyntaxTrees)
             {
@@ -79,5 +93,47 @@ namespace Ptarmigan.Utils.Roslyn
                 }
             }
         }
+
+        // https://stackoverflow.com/questions/69636558/determining-if-a-private-field-is-read-using-roslyn
+        private static bool IsFieldRead(SyntaxNodeAnalysisContext context, IFieldSymbol fieldSymbol)
+        {
+            var classDeclarationSyntax = context.Node.Parent;
+            while (!(classDeclarationSyntax is ClassDeclarationSyntax))
+            {
+                classDeclarationSyntax = classDeclarationSyntax.Parent;
+                if (classDeclarationSyntax == null)
+                {
+                    throw new InvalidOperationException("You have somehow traversed up and out of the syntax tree when determining if a private member field is being read.");
+                }
+            }
+
+            //get all methods in the class
+            var methodsInClass = classDeclarationSyntax.DescendantNodes().OfType<MethodDeclarationSyntax>();
+            foreach (var method in methodsInClass)
+            {
+                //get all member references in those methods
+                if (context.SemanticModel.GetOperation(method).Descendants().OfType<IMemberReferenceOperation>().Any(x => x.Member.Equals(fieldSymbol)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // https://stackoverflow.com/questions/44142421/roslyn-check-if-field-declaration-has-been-assigned-to
+        // https://stackoverflow.com/questions/64009302/roslyn-c-how-to-get-all-fields-and-properties-and-their-belonging-class-acce/64014939#64014939
+        public static IEnumerable<IMemberReferenceOperation> GetMemberReferenceOperations(SemanticModel model,
+            SyntaxNode node)
+        {
+            return model.GetOperation(node).Descendants().OfType<IMemberReferenceOperation>();
+        }
+
+        public static bool IsPublicMember(this ISymbol symbol)
+            => symbol.DeclaringSyntaxReferences.Any(dsr => dsr.GetSyntax().IsPublicMember());
+
+        public static bool IsPublicMember(this SyntaxNode node)
+            => node is MemberDeclarationSyntax mds
+               && mds.Modifiers.Any(st => st.Kind() == SyntaxKind.PublicKeyword);
     }
 }
